7e3f94d59210e83d37b1f561071399384b8e6625
h-kanazawa <kanazawahotaka@gmail.com>
2018-02-17 20:32:04 +0000

    11.4 Open Addressing

--- src/hash_table.py
+++ src/hash_table.py
@@ -4,10 +4,45 @@ from typing import Tuple, List
 from copy import deepcopy
 
 
+def gen_division_hash(m: int):
+    def h(k: int):
+        return k % m
+    return h
+
+
+def gen_multiplication_hash(m: int):
+    # 0 < A < 1
+    # A should be "s / 2^ω" (0 < s < 2^ω)
+    # Knuth suggests the (5^0.5 - 2) / 2 is reasonable.
+    A = 2654435769.0 / (2 ^ 32)
+
+    def h(k: int):
+        return int((k * A % 1) * m)
+    return h
+
+
+def gen_lenear_probing(m: int, f):
+    """
+    f: auxiliary hash function
+    """
+    def h(k: int, i: int):
+        return (f(k) + i) % m
+    return h
+
+
 class X:
     def __init__(self, k, v):
         self.key = k
         self.value = v
+
+    def stringify(self):
+        return '(k: {}, v: {})'.format(self.key, self.value)
+
+    def __str__(self):
+        return self.stringify()
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self.key == other.key and self.value == other.value
 
 
 class DirectAddressTable:
@@ -34,8 +69,8 @@ class DirectAddressTable:
         self.T[x.key] = None
 
 
-class HashTable:
-    """hash table implemented with array, not dict.
+class ClosedAddressingHashTable:
+    """cloased addressing hash table implemented with array, not dict.
     Each chain of a HashTable is Array, but Linked List is used in the textbook.
     """
     def __init__(self, m: int, hash):
@@ -60,36 +95,76 @@ class HashTable:
         """
         self.T[self.hash(x.key)].append(x)
 
-    def delete(self, x: X):
+    def delete(self, key):
         """
         """
-        original = deepcopy(self.T[self.hash(x.key)])
-        self.T[self.hash(x.key)] = list(filter(lambda z: z.key != x.key, original))
+        original = deepcopy(self.T[self.hash(key)])
+        self.T[self.hash(key)] = list(filter(lambda z: z.key != key, original))
 
     def __str__(self):
         return '\n'.join(
-            [', '.join(
-                ['(k: {}, v: {})'.format(kv.key, kv.value) for kv in kvs]
-            ) for kvs in self.T]
+            [', '.join([kv.stringify() for kv in kvs]) for kvs in self.T]
         )
 
 
-def gen_division_hash(m: int):
-    def h(k: int):
-        return k % m
-    return h
+class OpenAddressingHashTable:
+    """open addressing hash table implemented with array, not dict.
 
+    hash: (key, int) => int
+    hash should return an int in range(0, m)
+    """
+    DELETED = 'DELETED'
 
-def gen_multiplication_hash(m: int):
-    # 0 < A < 1
-    # A should be "s / 2^ω" (0 < s < 2^ω)
-    # Knuth suggests the (5^0.5 - 2) / 2 is reasonable.
-    A = 2654435769.0 / (2 ^ 32)
+    def __init__(self, m: int, hash):
+        self.T = [None for i in range(m)]
+        self.m = m
+        self.hash = hash
 
-    def h(k: int):
-        return int((k * A % 1) * m)
-    return h
+    def insert(self, x: X):
+        for i in range(0, self.m):
+            j = self.hash(x.key, i)
+            if self.T[j] is None or self.T[j] == OpenAddressingHashTable.DELETED:
+                self.T[j] = x
+                break
+        else:
+            raise Exception('overflow')
+
+    def search(self, key):
+        for i in range(0, self.m):
+            j = self.hash(key, i)
+            if self.T[j] is None:
+                break
+            if self.T[j] == OpenAddressingHashTable.DELETED:
+                continue
+            if self.T[j].key == key:
+                return self.T[j]
+        return None
+
+    def delete(self, key):
+        for i in range(0, self.m):
+            j = self.hash(key, i)
+            if self.T[j] is None:
+                break
+            if self.T[j] == OpenAddressingHashTable.DELETED:
+                continue
+            if self.T[j].key == key:
+                self.T[j] = OpenAddressingHashTable.DELETED
+                break
+
+    def stringify(self):
+        st = []
+        for kv in self.T:
+            if kv is None:
+                st.append('')
+            elif kv == OpenAddressingHashTable.DELETED:
+                st.append('DELETED')
+            else:
+                st.append(kv.stringify())
+        return '\n'.join(st)
+
+    def __str__(self):
+        return self.stringify()
 
 
 if __name__ == '__main__':
-    print('x')
+    None
--- tests/test_hash_table.py
+++ tests/test_hash_table.py
@@ -1,3 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from src.hash_table import *
+
+
+def test_OpenAddressingHashTable():
+    m = 4
+    f = gen_division_hash(m)
+    h = gen_lenear_probing(m, f)
+    T = OpenAddressingHashTable(m, h)
+
+    T.insert(X(1, 10))
+    T.insert(X(2, 20))
+    T.insert(X(5, 30))
+    T.delete(2)
+    assert T.T[2] == OpenAddressingHashTable.DELETED
+
+    T.insert(X(9, 30))
+    assert T.T[0] is None
+    assert T.T[1] == X(1, 10)
+    assert T.T[2] == X(9, 30)
+    assert T.T[3] == X(5, 30)
+
+    assert T.search(1) == X(1, 10)
+    assert T.search(2) is None
+    assert T.search(5) == X(5, 30)
+    assert T.search(9) == X(9, 30)
