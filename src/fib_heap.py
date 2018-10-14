336102649ce10e37925675b18ed2edd59a7f2630
h-kanazawa <kanazawahotaka@gmail.com>
2018-10-14 23:11:03 +0100

    19.2 Fibonacci Heap extract min

--- src/fib_heap.py
+++ src/fib_heap.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from math import floor, log
+
 
 class X:
     def __init__(self, k, v, degree=0, mark=False, p=None, c=None):
@@ -44,11 +46,24 @@ class X:
             curr = curr.l
         return xs
 
+    def children(self):
+        """show children as list
+        """
+        if self.c is None:
+            return []
+        return self.c.siblings_r()
+
     def add_to_right(self, x):
         x.r = self.r
         x.l = self
         self.r.l = x
         self.r = x
+
+    def add_to_left(self, x):
+        x.l = self.l
+        x.r = self
+        self.l.r = x
+        self.l = x
 
     def link_list(self, x):
         self.r.l = x.l
@@ -56,14 +71,28 @@ class X:
         self.r = x
         x.l = self
 
+    def remove_myself_from_siblings(self):
+        if self.r == self:
+            return
+        self.l.r = self.r
+        self.r.l = self.l
+        self.l = self
+        self.r = self
+
+    def add_child(self, y):
+        if self.d == 0:
+            self.c = y
+        else:
+            self.c.add_to_right(y)
+        self.d += 1
+        y.p = self
+
     def stringify(self):
-        return 'k: {}, v: {}, p, {} {}: c: {} {}, l: {} {}, r: {} {}, degree: {}, mark: {}'.format(
+        return 'k: {}, v: {}, p, {}: c: {}, l: ({}, {}), r: ({}, {}), degree: {}, mark: {}'.format(
             self.k,
             self.v,
-            self.p.k,
-            self.p.v,
-            self.c.k,
-            self.c.v,
+            'None' if self.p is None else '({}, {})'.format(self.p.k, self.p.v),
+            'None' if self.c is None else '({}, {})'.format(self.c.k, self.c.v),
             self.l.k,
             self.l.v,
             self.r.k,
@@ -83,6 +112,7 @@ class FibHeap:
         page 424
         """
         self.min = None
+        # the number of nodes
         self.n = 0
 
     def root_list(self):
@@ -107,9 +137,73 @@ class FibHeap:
                 self.min = x
         self.n += 1
 
+    def extract_min(self):
+        z = self.min
+
+        if z is None:
+            return None
+
+        if z.c is not None:
+            # add z.children to root_list
+            child = z.c
+            z.c = None
+
+            for c in child.siblings_r():
+                c.p = None
+
+            z.l.link_list(child)
+
+        # remove z from root_list
+        if z == z.r:
+            self.min = None
+        else:
+            self.min = z.r
+            z.remove_myself_from_siblings()
+            self.consolidate()
+        self.n -= 1
+        return z
+
+    def consolidate(self):
+        Dn = D(self.n)
+        A = [None for _ in range(Dn + 1)]
+        for w in self.root_list():
+            x = w
+            d = x.d
+            while A[d] is not None:
+                # y is a node whose degree is same with x
+                y = A[d]
+                if x.k > y.k:
+                    tmp = x
+                    x = y
+                    y = tmp
+                # at this point, x.k < y.k is guarantieed
+                self.link(y, x)
+                A[d] = None
+                d += 1
+            A[d] = x
+        self.min = None
+
+        for i in range(Dn + 1):
+            if A[i] is not None:
+                A[i].l = A[i]
+                A[i].r = A[i]
+                if self.min is None:
+                    self.min = A[i]
+                else:
+                    self.min.add_to_left(A[i])
+                    if A[i].k < self.min.k:
+                        self.min = A[i]
+
+    def link(self, y, x):
+        y.remove_myself_from_siblings()
+        x.add_child(y)
+        y.mark = False
+
 
 def union(H1, H2):
     """union fib heaps
+
+    amortized cost: O(1)
 
     Arguments:
         H1 {FibHeap}
@@ -124,6 +218,18 @@ def union(H1, H2):
         H.min = H1.min
     H.n = H1.n + H2.n
     return H
+
+
+def D(n):
+    """maximum degree
+
+    Arguments:
+        n {float} the number of nodes
+
+    Returns:
+        int
+    """
+    return floor(log(n, 2))
 
 
 if __name__ == '__main__':
--- tests/test_fib_heap.py
+++ tests/test_fib_heap.py
@@ -3,6 +3,11 @@
 from src.fib_heap import *
 
 
+################################################################
+# Tests for X
+################################################################
+
+
 def test_add_to_right_with_1_element():
     # original [x1]
     x1 = X(1, 'x1')
@@ -81,6 +86,50 @@ def test_link_list():
     assert x1.siblings_l() == [x3, x5, x2, x4, x1]
 
 
+def test_remove_myself_from_siblings():
+    x1 = X(1, 'x1')
+    x2 = X(2, 'x2')
+    x3 = X(3, 'x3')
+    x4 = X(4, 'x4')
+    x5 = X(5, 'x5')
+    x1.add_to_right(x4)
+    x1.add_to_right(x2)
+    x1.add_to_right(x5)
+    x1.add_to_right(x3)
+    assert x1.siblings_r() == [x1, x3, x5, x2, x4]
+
+    x3.remove_myself_from_siblings()
+    assert x3.siblings_r() == [x3]
+    assert x1.siblings_r() == [x1, x5, x2, x4]
+
+    x1.remove_myself_from_siblings()
+    assert x1.siblings_r() == [x1]
+    assert x2.siblings_r() == [x2, x4, x5]
+
+
+def add_child():
+    x1 = X(1, 'x1')
+    x2 = X(2, 'x2')
+    x3 = X(3, 'x3')
+    x1.add_child(x2)
+    assert x1.c == x2
+    assert x2.p == x1
+    assert x1.d == 1
+
+    x1.add_child(x3)
+    assert x1.c == x2
+    assert x2.p == x1
+    assert x3.p == x1
+    assert x1.d == 2
+    assert x2.siblings_r == [x2, x3]
+    assert x2.siblings_l == [x3, x2]
+
+
+################################################################
+# Tests for FibHeap
+################################################################
+
+
 def test_init():
     fh = FibHeap()
     assert fh.min == None
@@ -97,18 +146,188 @@ def test_insert():
 
     fh.insert(x2)
     assert fh.n == 1
-    assert fh.min.v == 'x2'
+    assert fh.min == x2
     assert fh.root_list() == [x2]
 
     fh.insert(x3)
     assert fh.n == 2
-    assert fh.min.v == 'x2'
+    assert fh.min == x2
     assert fh.root_list() == [x2, x3]
 
     fh.insert(x1)
     assert fh.n == 3
-    assert fh.min.v == 'x1'
+    assert fh.min == x1
     assert fh.root_list() == [x1, x3, x2]
+
+
+def test_extract_min_if_heap_is_empty():
+    fh = FibHeap()
+    z = fh.extract_min()
+    assert z == None
+
+
+def test_extract_min_if_heap_has_one_root_with_no_children():
+    x1 = X(1, 'x1')
+    fh = FibHeap()
+    fh.insert(x1)
+    z = fh.extract_min()
+    assert z == x1
+    assert fh.min == None
+    assert fh.root_list() == []
+
+
+def test_extract_min_with_no_children():
+    x1 = X(1, 'x1')
+    x2 = X(2, 'x2')
+    x3 = X(3, 'x3')
+    x4 = X(4, 'x4')
+    x5 = X(5, 'x5')
+
+    fh = FibHeap()
+    fh.insert(x1)
+    fh.insert(x4)
+    fh.insert(x2)
+    fh.insert(x5)
+    fh.insert(x3)
+    assert fh.root_list() == [x1, x3, x5, x2, x4]
+
+    z = fh.extract_min()
+    """
+    expect
+    [2]   ...root_list
+     ↕↖
+    [4↔3]
+       ↕
+      [5]
+    """
+    assert z == x1
+    assert fh.min == x2
+    assert fh.n == 4
+
+    assert x2.d == 2
+    assert x2.children() == [x4, x3]
+    assert x2.p == None
+
+    assert x3.d == 1
+    assert x3.children() == [x5]
+    assert x3.p == x2
+
+    assert x4.d == 0
+    assert x4.children() == []
+    assert x4.p == x2
+
+    assert x5.d == 0
+    assert x5.children() == []
+    assert x5.p == x3
+
+
+def test_extract_min_with_children():
+    """
+    Fig 19.4
+    page 428
+    """
+    x23 = X(23, 'x23')
+    x7 = X(7, 'x7')
+    x21 = X(21, 'x21')
+    x3 = X(3, 'x3')
+    x18 = X(18, 'x18')
+    x39 = X(39, 'x39')
+    x52 = X(52, 'x52')
+    x38 = X(38, 'x38')
+    x41 = X(41, 'x41')
+    x17 = X(17, 'x17')
+    x30 = X(30, 'x30')
+    x24 = X(24, 'x24')
+    x26 = X(26, 'x26')
+    x35 = X(35, 'x35')
+    x46 = X(46, 'x46')
+
+    x18.mark = True
+    x39.mark = True
+    x26.mark = True
+
+    x23.add_to_right(x7)
+    x7.add_to_right(x21)
+    x21.add_to_right(x3)
+    x3.add_to_right(x17)
+    x17.add_to_right(x24)
+
+    x3.add_child(x18)
+    x3.add_child(x38)
+    x3.add_child(x52)
+    x18.add_child(x39)
+    x38.add_child(x41)
+    x17.add_child(x30)
+    x24.add_child(x26)
+    x24.add_child(x46)
+    x26.add_child(x35)
+
+    fh = FibHeap()
+    fh.insert(x3)
+    fh.n = 15
+
+    fh.extract_min()
+
+    print([r.k for r in fh.root_list()])
+    assert fh.n == 14
+    assert fh.min == x7
+    assert fh.root_list() == [x7, x18, x38]
+
+    assert x7.children() == [x23, x24, x17]
+    assert x7.d == 3
+    assert x7.p is None
+
+    assert x23.children() == []
+    assert x23.d == 0
+    assert x23.p == x7
+
+    assert x24.children() == [x26, x46]
+    assert x24.d == 2
+    assert x24.p == x7
+
+    assert x46.children() == []
+    assert x46.d == 0
+    assert x46.p == x24
+
+    assert x26.children() == [x35]
+    assert x26.d == 1
+    assert x26.p == x24
+
+    assert x35.children() == []
+    assert x35.d == 0
+    assert x35.p == x26
+
+    assert x17.children() == [x30]
+    assert x17.d == 1
+    assert x17.p == x7
+
+    assert x30.children() == []
+    assert x30.d == 0
+    assert x30.p == x17
+
+    assert x18.children() == [x39, x21]
+    assert x18.d == 2
+    assert x18.p is None
+
+    assert x39.children() == []
+    assert x39.d == 0
+    assert x39.p == x18
+
+    assert x21.children() == [x52]
+    assert x21.d == 1
+    assert x21.p == x18
+
+    assert x52.children() == []
+    assert x52.d == 0
+    assert x52.p == x21
+
+    assert x38.children() == [x41]
+    assert x38.d == 1
+    assert x38.p is None
+
+    assert x41.children() == []
+    assert x41.d == 0
+    assert x41.p == x38
 
 
 def test_union():
